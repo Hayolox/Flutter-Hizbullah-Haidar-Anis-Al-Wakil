a76b972e17d21cd20d456711c99d5ea6a29167f9
Hayolox <akiklii72@gmail.com>
2022-02-22 10:43:15 +0700

    delete section 5

--- 5_Basic Dart Programming/praktikum/addstring.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  String firstName = 'Hizbullah ';
-  String middleName = 'haidar ';
-  String lastName = 'Anis Al Wakil ';
-
-  String fullName = firstName + middleName + lastName;
-
-  print(fullName);
-}
--- 5_Basic Dart Programming/praktikum/luaslingkaran.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main() {
-  const phi = 3.14;
-  double r = 2;
-  double luas = phi * r * r;
-  print(luas);
-}
