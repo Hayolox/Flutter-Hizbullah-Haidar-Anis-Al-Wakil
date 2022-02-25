f86ff49527d3fe55bab1b734f360cea2c2d584f0
Hayolox <akiklii72@gmail.com>
2022-02-25 18:19:13 +0700

    update section 7

--- 7_Advance Function - Async-Await/praktikum/ListAndMap.dart
+++ 7_Advance Function - Async-Await/praktikum/ListAndMap.dart
@@ -1,22 +1,23 @@
-
 void main() {
-    
+  // List hewan yang berisi nama-nama hesan
   var hewan = [
- ['anjing', 'ayam',], 
- ['semut', 'ular'], 
- ];
+    [
+      'anjing',
+      'ayam',
+    ],
+    ['semut', 'ular'],
+  ];
 
- var nameHewan = {
-    'hewan1' : hewan[0][1],
-    'hewan2' : hewan[0][0],
-    'hewan3' : hewan[1][0],
-    'hewan4' : hewan[1][1],
- };
+// Map nameHewan yang isi key tersebut dari list
+  var nameHewan = {
+    'hewan1': hewan[0][1],
+    'hewan2': hewan[0][0],
+    'hewan3': hewan[1][0],
+    'hewan4': hewan[1][1],
+  };
 
- for (var key in nameHewan.keys){
-   print(nameHewan[key]);
- }
-
-
-
+  //Perulangan untuk menampilkan MAP
+  for (var key in nameHewan.keys) {
+    print(nameHewan[key]);
+  }
 }
