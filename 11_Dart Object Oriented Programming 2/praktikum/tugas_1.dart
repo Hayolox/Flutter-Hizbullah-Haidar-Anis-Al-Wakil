1b78c6b1d36ee0500d3df00bc72a83c5a2a6d248
Hayolox <akiklii72@gmail.com>
2022-02-26 06:15:55 +0700

    update section 11

---
+++ 11_Dart Object Oriented Programming 2/praktikum/tugas_1.dart
@@ -0,0 +1,48 @@
+class BangungRuang {
+  // Variable global yang dimana masih belum mememiliki nilai
+  late double panjang;
+  late double lebar;
+  late double tinggi;
+
+  //Function volume yang dimana variable global sudah memiliki nilai
+  void volume(double paramPanjang, double paramLebar, double paramTinggi) {
+    panjang = paramPanjang;
+    lebar = paramLebar;
+    tinggi = paramTinggi;
+  }
+}
+
+//Function kubus, disini rumus untuk menghitung kubus
+class Kubus extends BangungRuang {
+  double sisi = 0;
+
+  void hitungKubus(double paramSisis) {
+    sisi = paramSisis;
+    var hasil = sisi * sisi * sisi;
+    print('---------Kubus----------');
+    print('');
+    print(hasil);
+  }
+}
+
+//Fucntion balok yang dimana pada function ini berisi rumus perhitungan kubus
+class Balok extends BangungRuang {
+  // void hitungBalok() {
+  //   double hasil = panjang * lebar * tinggi;
+
+  //   print('---------Balok----------');
+  //   print('');
+  //   print(hasil);
+  // }
+}
+
+void main() {
+  @override
+  var getKubus = Kubus();
+  getKubus.hitungKubus(4);
+
+  // @override
+  // var getBalok = Balok();
+  // getBalok.volume(3, 3, 3);
+  // getBalok.hitungBalok();
+}
