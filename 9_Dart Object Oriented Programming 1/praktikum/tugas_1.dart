64fa1858a516390916d6a87f5081e233eb415571
Hayolox <akiklii72@gmail.com>
2022-02-25 18:23:40 +0700

    update section 9

--- 9_Dart Object Oriented Programming 1/praktikum/tugas_1.dart
+++ 9_Dart Object Oriented Programming 1/praktikum/tugas_1.dart
@@ -1,37 +1,33 @@
-
-class Hewan{
-  List<int> berat = [20,30,40];
-
+class Hewan {
+  List<int> berat = [20, 30, 40];
 }
 
-class Mobil{
-
+class Mobil {
   // Jika muatan lebih dar atau sama dengan kapasistas, maka akan menampilkan pesan "Muatan Sudah Penuh"
-    List<String> muatan = ['anjing','kucing','buaya','ayam'];
+  List<String> muatan = ['anjing', 'kucing', 'buaya', 'ayam'];
 
-    void tambahMuatan(){
-      int jumlahMuatan = muatan.length;
-      int kapasitas = 4;
-     
-   
+  /*Function tambahan yang dimana isinya adalah kondisi untung 
+  menghitung muatan, Apakah melebihi kapasitas atau tidak*/
+  void tambahMuatan() {
+    int jumlahMuatan = muatan.length;
+    int kapasitas = 4;
 
-      if (jumlahMuatan >= kapasitas ){
-         print('Muatan Sudah Penuh');
-      }else{
-           muatan.add('babi');
-      }
-      print(muatan);
-
-    } 
-
+    /* 
     
+    Kondisi apabial muatan sama dengan atau lebih dari 
+    kapasitans maka muatan sudah penuh kalo tidak muatan akan ditamabah
+
+    */
+    if (jumlahMuatan >= kapasitas) {
+      print('Muatan Sudah Penuh');
+    } else {
+      muatan.add('babi');
+    }
+    print(muatan);
+  }
 }
 
-
- void main() {
+void main() {
   var mobil = Mobil();
   mobil.tambahMuatan();
-
-
- 
 }
--- 9_Dart Object Oriented Programming 1/praktikum/tugas_2.dart
+++ 9_Dart Object Oriented Programming 1/praktikum/tugas_2.dart
@@ -1,37 +1,43 @@
-
-class Hewan{
-  List<int> berat = [20,30,40];
-
+class Hewan {
+  List<int> berat = [20, 30, 40];
 }
 
-class Mobil{
-    List<String> muatan = ['anjing','kucing','buaya','ayam',];
+class Mobil {
+  // List yang berisi nama-nama hewan
+  List<String> muatan = [
+    'anjing',
+    'kucing',
+    'buaya',
+    'ayam',
+  ];
 
-     void tambahMuatan(){
-      int jumlahMuatan = muatan.length;
-      int kapasitas = 4;
-     
-   
+  /*Function tambahan yang dimana isinya adalah kondisi untung 
+  menghitung muatan, Apakah melebihi kapasitas atau tidak*/
+  void tambahMuatan() {
+    int jumlahMuatan = muatan.length;
+    int kapasitas = 4;
 
-      if (jumlahMuatan >= kapasitas ){
-         print('Muatan Sudah Penuh');
-      }else{
-           muatan.add('babi');
-      }
-      print(muatan);
+    /* 
+    
+    Kondisi apabial muatan sama dengan atau lebih dari 
+    kapasitans maka muatan sudah penuh kalo tidak muatan akan ditamabah
 
-    } 
-    void totalMuatan(){
-      print('total muatan mobil adalah ${muatan.length}');
+    */
+    if (jumlahMuatan >= kapasitas) {
+      print('Muatan Sudah Penuh');
+    } else {
+      muatan.add('babi');
     }
+    print(muatan);
+  }
+
+  void totalMuatan() {
+    print('total muatan mobil adalah ${muatan.length}');
+  }
 }
 
-
- void main() {
-   
+void main() {
   var mobil = Mobil();
   mobil.tambahMuatan();
   mobil.totalMuatan();
-
- 
 }
