65943261fbe01039396319e0d7732c7ce57ad7bd
Hayolox <akiklii72@gmail.com>
2022-02-28 20:44:46 +0700

    update section 14

---
+++ 14_Flutter Layout/praktikum/ListView/lib/data.dart
@@ -0,0 +1,42 @@
+List<Map<String, dynamic>> getData = [
+  {
+    'name': 'Leanne Graham',
+    'number': '1-770-736-8031 x56442',
+    'profile': 'L',
+  },
+  {
+    'name': 'Erwin Howel',
+    'number': '010-921-6593 x09125',
+    'profile': 'E',
+  },
+  {
+    'name': 'Cilementine Bauch',
+    'number': '1-463-123-4447',
+    'profile': 'C',
+  },
+  {
+    'name': 'Patricia Lebsack',
+    'number': '493-170-9623 x156',
+    'profile': 'P',
+  },
+  {
+    'name': 'Chelsey Dietrich',
+    'number': '(254)954-1289',
+    'profile': 'C',
+  },
+  {
+    'name': 'Cilementine Bauch',
+    'number': '1-463-123-4447',
+    'profile': 'C',
+  },
+  {
+    'name': 'Mrs. Dennis Schulisth',
+    'number': '1-447-935-8478 x6430',
+    'profile': 'M',
+  },
+  {
+    'name': 'Kurtis Weisnnat',
+    'number': '210.607.6132',
+    'profile': 'K',
+  },
+];
--- 14_Flutter Layout/praktikum/ListView/lib/main.dart
+++ 14_Flutter Layout/praktikum/ListView/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:section15/data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,75 +12,57 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('JSON ListView in Flutter || Hizbullah Haidar A A'),
-        ),
-        body: ListView(
-          children: [
-            Content('Leanne Graham', '1-770-736-8031 x56442', 'L'),
-            Content('Erwin Howell', '010-921-6593 x09125', 'E'),
-            Content('Cilementine Bauch', '1-463-123-4447', 'L'),
-            Content('Patricia Lebsack', '493-170-9623 x156', 'P'),
-            Content('Chelsey Dietrich', '(254)954-1289', 'C'),
-            Content('Mrs. Dennis Schulist', '1-447-935-8478 x6430', 'M'),
-            Content('Kurtis Weisnnat', '210.607.6132', 'K'),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Content extends StatelessWidget {
-  Content(this.name, this.number, this.profile);
-  final String name;
-  final String number;
-  final String profile;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      child: Row(
-        children: [
-          Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-              color: Colors.green,
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Center(
-              child: Text(
-                profile,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-              ),
-            ),
+          appBar: AppBar(
+            title:
+                const Text('JSON ListView in Flutter || Hizbullah Haidar A A'),
           ),
-          const SizedBox(
-            width: 10,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                name,
-                style: const TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              Text(
-                number,
-                style: const TextStyle(
-                  color: Colors.grey,
-                ),
-              )
-            ],
-          )
-        ],
-      ),
+          body: ListView.builder(
+              itemCount: getData.length,
+              itemBuilder: (context, index) => Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 10),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: Center(
+                            child: Text(
+                              getData[index]['profile'],
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              getData[index]['name'],
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(
+                              getData[index]['number'],
+                              style: const TextStyle(
+                                color: Colors.grey,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ))),
     );
   }
 }
