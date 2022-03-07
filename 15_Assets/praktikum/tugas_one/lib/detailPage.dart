f358e274ff5fc72594b025e88f86b9a3dd2ddd7b
Hayolox <akiklii72@gmail.com>
2022-03-07 09:53:00 +0700

    update section 15

---
+++ 15_Assets/praktikum/tugas_one/lib/detailPage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  late String dataDetail = '';
+  DetailPage(this.dataDetail);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Akil'),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(dataDetail), fit: BoxFit.cover)),
+      ),
+    );
+  }
+}
---
+++ 15_Assets/praktikum/tugas_one/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:tugas_one/detailPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  List data = [
+    'https://picsum.photos/seed/picsum/200/100',
+    'https://picsum.photos/id/237/200/100'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Taks 15 | Hizbullah A A'),
+          centerTitle: true,
+        ),
+        body: Home(data: [
+          'https://picsum.photos/seed/picsum/1000/1000',
+          'https://picsum.photos/id/237/1000/1000'
+        ]),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  final List data;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        itemCount: data.length,
+        itemBuilder: (BuildContext ctx, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) {
+                  return DetailPage(data[index]);
+                },
+              ));
+            },
+            child: Container(
+              margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Colors.grey,
+                  image: DecorationImage(
+                    image: NetworkImage(data[index]),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(15)),
+            ),
+          );
+        });
+  }
+}
